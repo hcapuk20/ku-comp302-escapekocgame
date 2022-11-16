b0e2355613a14b50fe49af99e8753e7ba231dc9b
hcapuk20 <hcapuk20@ku.edu.tr>
2022-11-16 22:56:53 +0300

    initial commit: main and GamePanel classes added.

---
+++ main/GamePanel.java
@@ -0,0 +1,46 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable{
+
+    final int originalTileSize = 16;
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // keep this for now
+    Thread gameThread;
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(768, 576));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+
+    public void startGame(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        while(gameThread != null){
+            // main game loop
+            // needs to update and display
+            update();
+
+            repaint(); // this actually calls paintComponent method -> do not ask me.
+
+        }
+
+    }
+
+    public void update(){
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+}
