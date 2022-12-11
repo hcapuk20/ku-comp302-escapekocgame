b1414a98c161e1cb5c0ba8c1bb65d375c8dac052
Bar2C <mcelasun20@ku.edu.tr>
2022-12-11 20:33:30 +0300

    shooter and time-wasting class implemented

---
+++ main/models/Alien/Shooter.java
@@ -0,0 +1,25 @@
+package main.models.Alien;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Shooter extends Alien{
+    public Shooter(int x, int y, int height, int width, String alien_type) {
+        this.locationX = x;
+        this.locationY = y;
+        this.height = height;
+        this.width = width;
+        try {
+            this.image = ImageIO.read(new File("assets/alien2.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        this.collidable = true;
+        int hitBoxIndex = width/6;
+        int hitBoxSize = width - (2 * hitBoxIndex);
+        this.collisionArea = new Rectangle(hitBoxIndex,hitBoxIndex,hitBoxSize,hitBoxSize);
+        this.alien_type = alien_type;
+    }
+}
